d44271c55cfa17cf6a0e70ff923bf4f191bcc6c7
FanqingM <71324430+FanqingM@users.noreply.github.com>
2021-10-07 17:53:11 +0800

    fix: 解决了适配器bug

--- Design_Pattern/src/test/java/musementpark/offline/base/flower/adapter/AdapterTest.java
+++ Design_Pattern/src/test/java/musementpark/offline/base/flower/adapter/AdapterTest.java
@@ -17,16 +17,4 @@ class AdapterTest {
         //使用适配器后
         winter.charge(aNormal);
     }
-//    public void chargeForPhone() {
-//        HomeBattery homeBattery = new HomeBattery();
-//        int homeVolt = homeBattery.supply();
-//        System.out.println("家庭电源提供的电压是 " + homeVolt + "V");
-//
-//        Adapter adapter = new Adapter();
-//        int chargeVolt = adapter.convert(homeVolt);
-//        System.out.println("使用适配器将家庭电压转换成了 " + chargeVolt + "V");
-//
-//        USBLine usbLine = new USBLine();
-//        usbLine.charge(chargeVolt);
-//    }
 }
