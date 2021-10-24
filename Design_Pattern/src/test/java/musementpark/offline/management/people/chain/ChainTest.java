1b95765514224db8412cd1ba393c672355aee8ff
FanqingM <71324430+FanqingM@users.noreply.github.com>
2021-10-24 12:25:23 +0800

    fix: 修改责任链模式测试代码

--- Design_Pattern/src/test/java/musementpark/offline/management/people/chain/ChainTest.java
+++ Design_Pattern/src/test/java/musementpark/offline/management/people/chain/ChainTest.java
@@ -9,25 +9,25 @@ class ChainTest {
     @Test
     public void test() {
         //创建一个树状结构
-        Component root = new Composite("总管理员");
+        Component root = new Composite("总管理部门");
         root.setParent(null);
         root.setLevel(3);
-        Component c1 = new Composite("中级管理员");
-        Component c2 = new Composite("中级管理员");
+        Component c1 = new Composite("维修部门");
+        Component c2 = new Composite("管理部门");
         c1.setLevel(2);
         c2.setLevel(2);
         root.addChild(c1);
         root.addChild(c2);
         c1.setParent(root);
         c2.setParent(root);
-        Component lf1 = new Leaf("低级管理员");
-        Component lf2 = new Leaf("低级管理员");
+        Component lf1 = new Leaf("基础设施维修部门");
+        Component lf2 = new Leaf("娱乐设施维修部门");
         c1.addChild(lf1);
         c1.addChild(lf2);
         lf1.setParent(c1);
         lf2.setParent(c1);
-        Component lf3 = new Leaf("低级管理员");
-        Component lf4 = new Leaf("低级管理员");
+        Component lf3 = new Leaf("清洁工管理部门");
+        Component lf4 = new Leaf("维修人员管理部门");
         lf1.setLevel(1);
         lf2.setLevel(1);
         lf3.setLevel(1);
--- Design_Pattern/src/test/java/musementpark/offline/management/people/mediator/MediatorTest.java
+++ Design_Pattern/src/test/java/musementpark/offline/management/people/mediator/MediatorTest.java
@@ -1,9 +1,42 @@
 package musementpark.offline.management.people.mediator;
 
+import musementpark.offline.management.people.composite.Component;
+import musementpark.offline.management.people.composite.Composite;
+import musementpark.offline.management.people.composite.Leaf;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MediatorTest {
     public static void main(String[] args) {
+        Component root = new Composite("总管理部门");
+        root.setParent(null);
+        root.setLevel(3);
+        Component c1 = new Composite("维修部门");
+        Component c2 = new Composite("管理部门");
+        c1.setLevel(2);
+        c2.setLevel(2);
+        root.addChild(c1);
+        root.addChild(c2);
+        c1.setParent(root);
+        c2.setParent(root);
+        Component lf1 = new Leaf("基础设施维修部门");
+        Component lf2 = new Leaf("娱乐设施维修部门");
+        c1.addChild(lf1);
+        c1.addChild(lf2);
+        lf1.setParent(c1);
+        lf2.setParent(c1);
+        Component lf3 = new Leaf("清洁工管理部门");
+        Component lf4 = new Leaf("维修人员管理部门");
+        lf1.setLevel(1);
+        lf2.setLevel(1);
+        lf3.setLevel(1);
+        lf4.setLevel(1);
+        c2.addChild(lf3);
+        c2.addChild(lf4);
+        lf3.setParent(c2);
+        lf4.setParent(c2);
+
+
         Colleague colleagueA = new ConcreteColleagueA();
         Colleague colleagueB = new ConcreteColleagueB();
 
