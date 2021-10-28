2a0685339a9bc46d428802afa0e6b44dd0d0dff5
FanqingM <71324430+FanqingM@users.noreply.github.com>
2021-10-28 23:58:42 +0800

    fix: 修改输出格式

--- Design_Pattern/src/main/java/musementpark/offline/base/visitor/CheckElectricVisitor.java
+++ Design_Pattern/src/main/java/musementpark/offline/base/visitor/CheckElectricVisitor.java
@@ -3,17 +3,33 @@ package musementpark.offline.base.visitor;
 
 import musementpark.offline.base.composite.Composite;
 import musementpark.offline.base.composite.Leaf;
+import musementpark.util.Print;
+import musementpark.util.PrintInfo;
 
 public class CheckElectricVisitor implements Visitor {
 
     @Override
     public void visitComposite(Composite composite) {
-        System.out.println("检查电量" + composite.getName());
+//        Print.print(
+//                new PrintInfo(
+//                        "CheckElectricVisitor",
+//                        String.valueOf(System.identityHashCode(this)),
+//                        "visitComposite",
+//                        "检查"+ composite.getName()+"电量"
+//                )
+//        );
     }
 
     @Override
     public void visitLeaf(Leaf leaf) {
-        System.out.println("检查电量" + leaf.getName());
+        Print.print(
+                new PrintInfo(
+                        "CheckElectricVisitor",
+                        String.valueOf(System.identityHashCode(this)),
+                        "visitLeaf",
+                        "检查"+ leaf.getName()+"电量"
+                )
+        );
     }
 }
 
--- Design_Pattern/src/main/java/musementpark/offline/base/visitor/CheckTemperatureVisitor.java
+++ Design_Pattern/src/main/java/musementpark/offline/base/visitor/CheckTemperatureVisitor.java
@@ -3,17 +3,33 @@ package musementpark.offline.base.visitor;
 
 import musementpark.offline.base.composite.Composite;
 import musementpark.offline.base.composite.Leaf;
+import musementpark.util.Print;
+import musementpark.util.PrintInfo;
 
 public class CheckTemperatureVisitor implements Visitor {
 
     @Override
     public void visitComposite(Composite composite) {
-        System.out.println("检查温度" + composite.getName());
+//        Print.print(
+//                new PrintInfo(
+//                        "CheckTemperatureVisitor",
+//                        String.valueOf(System.identityHashCode(this)),
+//                        "visitComposite",
+//                        "检查" + composite.getName()+ "温度"
+//                )
+//        );
     }
 
     @Override
     public void visitLeaf(Leaf leaf) {
-        System.out.println("检查温度" + leaf.getName());
+        Print.print(
+                new PrintInfo(
+                        "CheckTemperatureVisitor",
+                        String.valueOf(System.identityHashCode(this)),
+                        "visitLeaf",
+                        "检查" + leaf.getName() + "温度"
+                )
+        );
     }
 }
 
