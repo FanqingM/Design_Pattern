df07c2e10312a18932a9ca2f50f50ba61c16098c
FanqingM <71324430+FanqingM@users.noreply.github.com>
2021-10-29 14:12:01 +0800

    fix: 修改迭代器模式

--- Design_Pattern/src/main/java/musementpark/offline/entertainment/iterator/Iterator.java
+++ Design_Pattern/src/main/java/musementpark/offline/entertainment/iterator/Iterator.java
@@ -1,9 +1,12 @@
-//package musementpark.offline.entertainment.iterator;
-///*
-//author: FanqingM
-//description: Iterator接口
-// */
-//public interface Iterator {
-//    public boolean hasNext();
-//    public Object next();
-//}
+package musementpark.offline.entertainment.iterator;
+
+import musementpark.offline.entertainment.composite.Component;
+
+/*
+author: FanqingM
+description: Iterator接口
+ */
+public interface Iterator {
+    public boolean hasNext();
+    public Component next();
+}
