878336e98eb285b4f9ff5351f5f41a7aa318f10b
hhhhzs <57512939+hhhhzs@users.noreply.github.com>
2021-10-30 20:38:13 +0800

    Add files via upload

---
+++ Design_Pattern/src/main/java/musementpark/offline/base/servant/CleaningServiced.java
@@ -0,0 +1,35 @@
+package musementpark.offline.base.service.servant;
+
+
+import musementpark.util.Print;
+import musementpark.util.PrintInfo;
+/**
+author: hzs
+description:清洁服务类
+ */
+public class CleaningServiced implements IServiced{
+    private boolean serviced = false;
+
+    /**
+     * 服务是否被执行
+     * @return boolean
+     */
+    public boolean getServiced(){
+        return serviced;
+    }
+
+    /**
+     * 执行服务
+     */
+    public void setServiced(){
+        serviced = true;
+        Print.print(
+                new PrintInfo(
+                        "CleaningServiced",
+                        String.valueOf(System.identityHashCode(this)),
+                        "setServiced",
+                        "清洁服务被执行"
+                )
+        );
+    }
+}
