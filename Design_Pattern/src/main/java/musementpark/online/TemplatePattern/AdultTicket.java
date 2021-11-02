0cee49da6b613084d82dc2d1f79e00de56872de1
dannyXSC <874794016@qq.com>
2021-11-02 23:20:04 +0800

    Merge branch 'Danny&Pandar' of https://github.com/FanqingM/Design_Pattern into Danny&Pandar

---
+++ Design_Pattern/src/main/java/musementpark/online/TemplatePattern/AdultTicket.java
@@ -0,0 +1,20 @@
+package musementpark.online.TemplatePattern;
+/**
+ * author: PandaLYZ
+ * description:该类为AdultTicket类，继承了Ticket类，处理成人票种相关信息
+ */
+public class AdultTicket extends Ticket {
+    /**
+     * description:构造函数，成人票是全价，没有折扣
+     * @param price 总价
+     */
+    AdultTicket(int price) {
+        super(price);
+    }
+
+    @Override
+    public double getDiscount() {
+        return 1;
+    }
+
+}
