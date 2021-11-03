68af0bd2d23f78b9382499f9e52329c869b59dd0
dannyXSC <874794016@qq.com>
2021-11-03 19:09:56 +0800

    更改 以适用于各种版本java

---
+++ Design_Pattern/src/main/java/musementpark/Main.java
@@ -0,0 +1,68 @@
+package musementpark;
+/*
+author: Fanqing_M
+description: 打印一个简单的UI
+*/
+
+import org.junit.platform.launcher.Launcher;
+import org.junit.platform.launcher.LauncherDiscoveryRequest;
+import org.junit.platform.launcher.TestIdentifier;
+import org.junit.platform.launcher.TestPlan;
+import org.junit.platform.launcher.core.LauncherDiscoveryRequestBuilder;
+import org.junit.platform.launcher.core.LauncherFactory;
+
+import java.util.Scanner;
+import java.util.Set;
+import java.lang.*;
+
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectPackage;
+import static org.junit.platform.engine.discovery.ClassNameFilter.includeClassNamePatterns;
+/**
+ * @author rand1925
+ *
+ * 测试主类，提供简单的交互界面
+ */
+public class Main {
+
+    /**
+     * 展示用例选单
+     *
+     * @param args 没有作用
+     */
+    public static void main(String[] args) {
+        LauncherDiscoveryRequestBuilder requestBuilder = LauncherDiscoveryRequestBuilder.request()
+//                .selectors(selectPackage(Main.class.getPackageName()))
+                .selectors(selectPackage("musementpark"))
+                .filters(includeClassNamePatterns(".*Test"));
+        LauncherDiscoveryRequest allTestRequest = requestBuilder.build();
+        Launcher launcher = LauncherFactory.create();
+        TestPlan testPlan = launcher.discover(allTestRequest);
+        Set<TestIdentifier> testIdentifierSet = testPlan.getChildren("[engine:junit-jupiter]");
+        TestIdentifier[] testIdentifierArray = testIdentifierSet.toArray(new TestIdentifier[0]);
+        for (int i = 0; i < testIdentifierArray.length; ++i) {
+            System.out.printf("%2d: %s\n", i, testIdentifierArray[i].getDisplayName());
+        }
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("输入一个数字选择特定测试， -1退出");
+            int choice = scanner.nextInt();
+            if (choice == -1) {
+                break;
+            }
+            if (choice < 0 || choice >= testIdentifierArray.length) {
+                System.out.println("越界！");
+                continue;
+            }
+            TestIdentifier chosenTestIdentifier = testIdentifierArray[choice];
+            System.out.printf("正在执行测试%2d: %s\n", choice, chosenTestIdentifier.getDisplayName());
+            LauncherDiscoveryRequest request = LauncherDiscoveryRequestBuilder.request()
+//                    .selectors(selectPackage(Main.class.getPackageName()))
+                    .selectors(selectPackage("musementpark"))
+                    .filters(includeClassNamePatterns(chosenTestIdentifier.getLegacyReportingName()))
+                    .build();
+            launcher.execute(request);
+        }
+    }
+}
+
+
